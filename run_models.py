80616ba6f4762b862d50aab6d030f77559c30429
Emil Sadek <esadek@hotmail.com>
2022-07-13 14:47:02 -0700

    Update: run_models.py

--- run_models.py
+++ run_models.py
@@ -4,10 +4,12 @@ from sqlalchemy import create_engine
 
 db = create_engine("dialect+driver://username:password@host:port/database")
 
-system("dbt run --exclude check_edges; dbt compile --select check_edges")
+system("dbt run --select queries edges; dbt compile --select check_edges")
 
 with open("target/compiled/id_stitching/models/check_edges.sql") as file:
     query = file.read()
 
 while db.execute(query).first()["count"]:
-    system("dbt run --select id_graph")
+    system("dbt run --select edges")
+
+system("dbt run --select id_graph")
