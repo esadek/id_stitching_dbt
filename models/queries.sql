8a6ee39240f3d1a6c6cf476bb59850baca302bd7
Emil Sadek <esadek@hotmail.com>
2022-07-15 16:46:24 -0700

    Remove: lower functions

--- models/queries.sql
+++ models/queries.sql
@@ -9,7 +9,7 @@ with columns as (
 )
 
 select
-    'select distinct lower(' || a.cn || ') as edge_a, lower(''' || a.cn  || ''') as edge_a_label, lower(' || b.cn || ') as edge_b, lower(''' || b.cn  || ''') as edge_b_label from ' ||  a.tn || ' where coalesce(' || a.cn ||  ', '''') <> '''' and coalesce(' || b.cn ||  ', '''') <> ''''' as sql_to_run
+    'select distinct ' || a.cn || '::text as edge_a, ''' || a.cn  || ''' as edge_a_label, ' || b.cn || '::text as edge_b, ''' || b.cn  || ''' as edge_b_label from ' ||  a.tn || ' where coalesce(' || a.cn ||  ', '''') <> '''' and coalesce(' || b.cn ||  ', '''') <> ''''' as sql_to_run
 from
     columns a
 inner join
